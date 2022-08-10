322718b3b187d78e62f107134dbb3fdadd416836
ugwuchukwuogbonnaya <stcasmir4excel@gmail.com>
2022-08-10 13:13:51 +0100

    added the controller api

--- src/main/java/com/casmir/org/controller/UserController.java
+++ src/main/java/com/casmir/org/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.casmir.org.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,12 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 	
+	@GetMapping("/home")
 	public String home() {
 		
 		return "Welcome To HOME PAGE";
 	}
 	
 	
+	@PostMapping("/dashboard")
 	public String dashboard() {
 		
 	return "Welcome To DASHBOARD PAGE";
