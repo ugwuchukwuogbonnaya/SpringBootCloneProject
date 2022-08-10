8205bd54fd9470baca892fe6b3513efc57c8ac76
ugwuchukwuogbonnaya <stcasmir4excel@gmail.com>
2022-08-10 12:19:57 +0100

    Added Spring Web Dependency

---
+++ src/main/java/com/casmir/org/SpringBootCloneProjectApplication.java
@@ -0,0 +1,13 @@
+package com.casmir.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCloneProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCloneProjectApplication.class, args);
+	}
+
+}
